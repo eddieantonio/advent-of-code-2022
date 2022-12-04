94653966f0486939bbb981ec02fdc91a4e5222cb
Eddie Antonio Santos <eddie.santos@ucdconnect.ie>
2022-12-04 11:18:21 +0000

    [day 4]: part 2

--- day-4/src/main.rs
+++ day-4/src/main.rs
@@ -11,16 +11,29 @@ impl SectionRange {
     fn fully_contains(self, other: SectionRange) -> bool {
         self.start <= other.start && self.end >= other.end
     }
+
+    fn overlaps(self, other: SectionRange) -> bool {
+        if self.start < other.start {
+            self.end >= other.start
+        } else {
+            other.end >= self.start
+        }
+    }
 }
 
 #[derive(Copy, Clone, Debug, Inpt)]
 #[inpt(regex = r",")]
-struct ElfPair(#[inpt(before)] SectionRange, #[inpt(after)] SectionRange);
+pub struct ElfPair(#[inpt(before)] SectionRange, #[inpt(after)] SectionRange);
 
 impl ElfPair {
-    fn has_fully_contained_section(self) -> bool {
+    pub fn has_fully_contained_section(self) -> bool {
         let ElfPair(first, second) = self;
         first.fully_contains(second) || second.fully_contains(first)
+    }
+
+    pub fn has_any_overlap(self) -> bool {
+        let ElfPair(first, second) = self;
+        first.overlaps(second)
     }
 }
 
@@ -28,7 +41,7 @@ impl ElfPair {
 fn main(pairs: Vec<ElfPair>) {
     let answer = pairs
         .into_iter()
-        .filter(|pair| pair.has_fully_contained_section())
+        .filter(|pair| pair.has_any_overlap())
         .count();
     println!("{answer}");
 }
