2bd8cb7ef9ac627f5528a186265e709e8235f6f8
Eddie Antonio Santos <eddie.santos@ucdconnect.ie>
2022-12-06 10:05:05 +0000

    [day 6]: part 2

--- day-6/src/main.rs
+++ day-6/src/main.rs
@@ -5,13 +5,17 @@ fn main() {
     let stdin = io::stdin();
     let buffer = stdin.lock().lines().next().unwrap().unwrap();
     let stream = buffer.trim();
-    let answer = start_packet(stream).unwrap();
+    let answer = start_message(stream).unwrap();
 
     println!("{answer}");
 }
 
 pub fn start_packet(s: &str) -> Option<usize> {
     start_distinct::<4>(s)
+}
+
+pub fn start_message(s: &str) -> Option<usize> {
+    start_distinct::<14>(s)
 }
 
 fn start_distinct<const PREFIX_SIZE: usize>(s: &str) -> Option<usize> {
@@ -36,11 +40,20 @@ fn start_distinct<const PREFIX_SIZE: usize>(s: &str) -> Option<usize> {
 mod test {
     use super::*;
     #[test]
-    fn test_examples() {
+    fn test_start_packet() {
         assert_eq!(Some(7), start_packet("mjqjpqmgbljsphdztnvjfqwrcgsmlb"));
         assert_eq!(Some(5), start_packet("bvwbjplbgvbhsrlpgdmjqwftvncz"));
         assert_eq!(Some(6), start_packet("nppdvjthqldpwncqszvftbrmjlhg"));
         assert_eq!(Some(10), start_packet("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg"));
         assert_eq!(Some(11), start_packet("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw"));
+    }
+
+    #[test]
+    fn test_start_message() {
+        assert_eq!(Some(19), start_message("mjqjpqmgbljsphdztnvjfqwrcgsmlb"));
+        assert_eq!(Some(23), start_message("bvwbjplbgvbhsrlpgdmjqwftvncz"));
+        assert_eq!(Some(23), start_message("nppdvjthqldpwncqszvftbrmjlhg"));
+        assert_eq!(Some(29), start_message("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg"));
+        assert_eq!(Some(26), start_message("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw"));
     }
 }
