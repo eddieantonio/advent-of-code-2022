415346e8f738e6425e7afb3a0f1ca55923cde346
Eddie Antonio Santos <eddie.santos@ucdconnect.ie>
2022-12-07 10:59:16 +0000

    [wip]: still awful

--- day-7/src/main.rs
+++ day-7/src/main.rs
@@ -1,20 +1,13 @@
 use inpt::{inpt, Inpt};
+use std::collections::VecDeque;
 use std::io::{self, BufRead};
 
 #[derive(Inpt, Debug)]
-#[inpt(regex = r"[$]\s+")]
-struct CommandLine(#[inpt(after)] Command);
-
-#[derive(Inpt, Debug)]
-enum Command {
-    #[inpt(regex = r"cd\s+(.+)")]
+enum Line {
+    #[inpt(regex = r"[$]\s+cd\s+(.+)")]
     Cd(String),
-    #[inpt(regex = r"ls")]
+    #[inpt(regex = r"[$]\s+ls")]
     Ls,
-}
-
-#[derive(Debug, Inpt)]
-enum Entry {
     #[inpt(regex = r"dir\s+(.+)")]
     Directory(String),
     #[inpt(regex = r"(\d+)\s+.+")]
@@ -23,47 +16,40 @@ enum Entry {
 
 fn main() {
     let stdin = io::stdin();
-    let lines: Vec<String> = stdin.lock().lines().filter_map(Result::ok).collect();
-    count_from_root(lines.into_iter());
+    let lines: Vec<_> = stdin
+        .lock()
+        .lines()
+        .filter_map(Result::ok)
+        .map(|line| inpt::<Line>(line.trim()).unwrap())
+        .collect();
+
+    println!("{lines:?}");
 }
 
-fn count_from_root(mut lines: impl Iterator<Item = String>) {
-    let first = lines.next().unwrap();
-    let Ok(CommandLine(Command::Cd(dir))) = inpt::<CommandLine>(&first) else {
-        panic!("First line bad: {first}")
+struct Calculation(VecDeque<Line>, usize);
+
+fn calculate_size(mut lines: VecDeque<Line>) -> Calculation {
+    let Some(Line::Ls) = lines.pop_front() else {
+        panic!("Did not see ls");
     };
 
-    if &dir != "/" {
-        panic!("Not root directory");
-    }
-
-    count_directory(lines);
-}
-
-/// Assumes:
-///  - previous line was $ cd <dir>
-///  - current line is ls
-///
-fn count_directory(mut lines: impl Iterator<Item = String>) -> usize {
-    let line = lines.next().unwrap();
-    let Ok(CommandLine(Command::Ls)) = inpt::<CommandLine>(&line) else {
-        panic!("Expected line to be ls but was: {line}")
-    };
-
-    let mut size = 0;
-
-    // parse directory entries
+    let mut current_size = 0;
     loop {
-        let Some(line) = lines.next() else {
-            // last line in the file
-            return size;
-        };
-
-        if let Ok(entry) = inpt::<Entry>(line.trim()) {
-            match entry {
-                Entry::File(f) => size += f,
-                Entry::Directory(_) => panic!("wat"),
+        let line = lines.pop_front();
+        match line {
+            Some(Line::Cd(dir)) if &dir == ".." => {
+                return Calculation(lines, current_size);
             }
+            Some(Line::File(size)) => current_size += size,
+            Some(Line::Directory(_)) => {
+                // Don't need to do anything for a directory
+            }
+            Some(Line::Cd(name)) => {
+                let Calculation(new_lines, size) = calculate_size(lines);
+                lines = new_lines;
+                current_size += size;
+            }
+            _ => unimplemented!("{line:?}, {lines:?}"),
         }
     }
 }
@@ -73,28 +59,95 @@ mod test {
     use super::*;
 
     #[test]
-    fn test_parse_command() {
-        assert!(matches!(inpt::<Command>("cd /"), Ok(Command::Cd(_))));
-        assert!(matches!(
-            inpt::<CommandLine>("$ cd /"),
-            Ok(CommandLine(Command::Cd(_)))
-        ));
+    fn test_parse_lines() {
+        use Line::*;
+        match inpt::<Line>("$ cd /") {
+            Ok(Cd(dir)) => assert_eq!("/", &dir),
+            err => panic!("{err:?}"),
+        }
 
-        if let Ok(CommandLine(Command::Cd(dir))) = inpt::<CommandLine>("$ cd /") {
-            assert_eq!("/", &dir);
+        match inpt::<Line>("$ ls") {
+            Ok(Ls) => (),
+            err => panic!("{err:?}"),
+        }
+
+        match inpt::<Line>("dir e") {
+            Ok(Directory(name)) => assert_eq!("e", &name),
+            err => panic!("{err:?}"),
+        }
+
+        match inpt::<Line>("62596 h.lst") {
+            Ok(File(size)) => assert_eq!(62596, size),
+            err => panic!("{err:?}"),
         }
     }
 
     #[test]
-    fn test_ls_output() {
-        assert!(matches!(inpt::<Entry>("dir e"), Ok(Entry::Directory(_))));
-        if let Ok(Entry::Directory(name)) = inpt::<Entry>("dir e") {
-            assert_eq!("e", &name);
-        }
+    fn test_leaf_directory() {
+        let lines: VecDeque<_> = vec!["$ ls", "584 i", "$ cd .."]
+            .into_iter()
+            .map(|line| inpt::<Line>(line).unwrap())
+            .collect();
 
-        assert!(matches!(inpt::<Entry>("62596 h.lst"), Ok(Entry::File(_))));
-        if let Ok(Entry::File(size)) = inpt::<Entry>("dir e") {
-            assert_eq!(62596, size);
-        }
+        let Calculation(lines, size) = calculate_size(lines);
+        assert_eq!(584, size);
+        assert_eq!(0, lines.len());
+    }
+
+    #[test]
+    fn test_nested_directories() {
+        let lines = vec![
+            "$ ls",
+            "dir e",
+            "29116 f",
+            "2557 g",
+            "62596 h.lst",
+            "$ cd e",
+            "$ ls",
+            "584 i",
+            "$ cd ..",
+            "$ cd ..",
+        ];
+        let lines: VecDeque<_> = lines
+            .into_iter()
+            .map(|line| inpt::<Line>(line).unwrap())
+            .collect();
+
+        let Calculation(lines, size) = calculate_size(lines);
+        assert_eq!(94853, size);
+        assert_eq!(0, lines.len());
+    }
+
+    #[test]
+    fn test_herp() {
+        let lines = vec![
+            "$ cd /",
+            "$ ls",
+            "dir a",
+            "14848514 b.txt",
+            "8504156 c.dat",
+            "dir d",
+            "$ cd a",
+            "$ ls",
+            "dir e",
+            "29116 f",
+            "2557 g",
+            "62596 h.lst",
+            "$ cd e",
+            "$ ls",
+            "584 i",
+            "$ cd ..",
+            "$ cd ..",
+            "$ cd d",
+            "$ ls",
+            "4060174 j",
+            "8033020 d.log",
+            "5626152 d.ext",
+            "7214296 k",
+        ];
+        let lines: Vec<_> = lines
+            .into_iter()
+            .map(|line| inpt::<Line>(line).unwrap())
+            .collect();
     }
 }
