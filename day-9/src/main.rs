a5ec31474bf721c3b1f260e6d3d84446cf9f3778
Eddie Antonio Santos <eddie.santos@ucdconnect.ie>
2022-12-09 10:40:59 +0000

    [day 9]: wip -- ascii art world

--- day-9/src/main.rs
+++ day-9/src/main.rs
@@ -12,47 +12,94 @@ enum Movement {
     Left(i32),
 }
 
+type Coords = (i32, i32);
+
+#[derive(Inpt, Debug)]
+struct World {
+    width: i32,
+    height: i32,
+    start: Coords,
+    head: Coords,
+    tail: Coords,
+}
+
 #[inpt::main]
 fn main(head_movement: Vec<Movement>) {
-    use Movement::*;
     println!("{head_movement:?}");
+    let world = World::from_bounds(&head_movement);
+    world.print();
+}
 
-    let mut min_width = 0;
-    let mut max_width = 1;
-    let mut min_height = 0;
-    let mut max_height = 1;
-    let mut x = 0;
-    let mut y = 0;
-
-    for m in head_movement {
-        match m {
-            Right(x_d) => x += x_d,
-            Left(x_d) => x -= x_d,
-            Up(y_d) => y -= y_d,
-            Down(y_d) => y += y_d,
-        }
-
-        if x >= max_width {
-            max_width = x + 1;
-        }
-        if x < min_width {
-            min_width = x;
-        }
-        if y >= max_height {
-            max_height = y + 1;
-        }
-
-        if y < min_height {
-            min_height = y;
+impl World {
+    fn print(&self) {
+        for y in 0..self.height {
+            for x in 0..self.width {
+                if (x, y) == self.head {
+                    print!("H");
+                } else if (x, y) == self.tail {
+                    print!("T");
+                } else if (x, y) == self.start {
+                    print!("s");
+                } else {
+                    print!(".");
+                }
+            }
+            println!();
         }
     }
 
-    let width = max_width - min_width;
-    let height = max_height - min_height;
-    println!("width: {width}");
-    println!("height: {height}");
-    println!("last pos: {x}, {y}");
-    let x = x - min_width;
-    let y = y - min_height;
-    println!("normalized: {x}, {y}");
+    fn from_bounds(head_movement: &[Movement]) -> Self {
+        use Movement::*;
+
+        let mut min_width = 0;
+        let mut max_width = 1;
+        let mut min_height = 0;
+        let mut max_height = 1;
+        let mut x = 0;
+        let mut y = 0;
+
+        for m in head_movement {
+            match m {
+                Right(x_d) => x += x_d,
+                Left(x_d) => x -= x_d,
+                Up(y_d) => y -= y_d,
+                Down(y_d) => y += y_d,
+            }
+
+            if x >= max_width {
+                max_width = x + 1;
+            }
+            if x < min_width {
+                min_width = x;
+            }
+            if y >= max_height {
+                max_height = y + 1;
+            }
+
+            if y < min_height {
+                min_height = y;
+            }
+        }
+
+        let width = max_width - min_width;
+        let height = max_height - min_height;
+        println!("width: {width}");
+        println!("height: {height}");
+        println!("last pos: {x}, {y}");
+        println!("{min_width}--{max_width}");
+        println!("{min_height}--{max_height}");
+        let x = x - min_width;
+        let y = y - min_height;
+        println!("normalized: {x}, {y}");
+        let start = (0 - min_width, 0 - min_height);
+        println!("start: {start:?}");
+
+        World {
+            width,
+            height,
+            start,
+            head: start,
+            tail: start,
+        }
+    }
 }
